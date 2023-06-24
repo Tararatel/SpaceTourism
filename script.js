6379e6f916ff59b2fbdec57056e6d6799f646506
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2023-06-24 23:40:31 +0300

    add data.json and update script.js

--- script.js
+++ script.js
@@ -10,19 +10,20 @@ burger.addEventListener('click', toggleMenu);
 closeBtn.addEventListener('click', toggleMenu);
 
 const planet = document.querySelector('.planet');
-console.log(planet);
+const technology = document.querySelector('.technology__wrapper');
+const crew = document.querySelector('.crew__wrapper');
 const url = '../../data.json';
 
-const getData = async () => {
+const getData = async (url) => {
 	const response = await fetch(url);
 	const data = await response.json();
 	return data;
 };
 
 try {
-	const data = await getData();
+	const data = await getData(url);
 	data.forEach((el) => {
-		if (el.id == planet.getAttribute('data-id')) {
+		if (el.id === +planet.getAttribute('data-id')) {
 			planet.insertAdjacentHTML(
 				'afterbegin',
 				`
@@ -51,6 +52,68 @@ try {
 						</div>
 					</div>
 				</div>
+			`
+			);
+		}
+	});
+} catch (error) {
+	console.log(error.messagr);
+}
+
+try {
+	const data = await getData(url);
+	data.forEach((el) => {
+		if (el.id === +technology.getAttribute('data-id')) {
+			technology.insertAdjacentHTML(
+				'afterbegin',
+				`
+				<img class="technology__wrapper__img_tablet" src="${el.imgSrcTablet}" alt="${el.title}">
+		  <nav class="technology__wrapper__links">
+		    <a class="technology__wrapper__links__link ${el.id === 5 ? 'technology__wrapper__links__link_active' : ''}" href="${
+					el.id === 5 ? '#' : './vehicle.html'
+				}">1</a>
+		    <a class="technology__wrapper__links__link ${el.id === 6 ? 'technology__wrapper__links__link_active' : ''}" href="${
+					el.id === 6 ? '#' : './spacePort.html'
+				}">2</a>
+		    <a class="technology__wrapper__links__link ${el.id === 7 ? 'technology__wrapper__links__link_active' : ''}" href="${
+					el.id === 7 ? '#' : './capsule.html'
+				}">3</a>
+		  </nav>
+		  <div class="technology__wrapper__content">
+		    <h3 class="technology__wrapper__content__terminology">THE TERMINOLOGY…</h3>
+		    <h2 class="technology__wrapper__content__title">${el.title}</h2>
+		    <p class="technology__wrapper__content__text">${el.description}</p>
+		  </div>
+		  <img class="technology__wrapper__img" src="${el.imgSrc}" alt="${el.title}" width="515">
+			`
+			);
+		}
+	});
+} catch (error) {
+	console.log(error.messagr);
+}
+
+try {
+	const data = await getData(url);
+	data.forEach((el) => {
+		if (el.id === +crew.getAttribute('data-id')) {
+			crew.insertAdjacentHTML(
+				'afterbegin',
+				`
+				<div class="crew__wrapper__content">
+					<h3 class="crew__wrapper__content__rank">${el.rank}</h3>
+					<h2 class="crew__wrapper__content__name">${el.name}</h2>
+					<p class="crew__wrapper__content__description">${el.description}</p>
+					<nav class="crew__wrapper__content__navigation">
+						<a href="${el.id === 8 ? '#' : './commander.html'}"><div class="round ${el.id === 8 ? 'round-active' : ''}"></div></a>
+						<a href="${el.id === 9 ? '#' : './missionSpecialist.html'}"><div class="round ${
+					el.id === 9 ? 'round-active' : ''
+				}"></div></a>
+						<a href="${el.id === 10 ? '#' : './pilot.html'}"><div class="round ${el.id === 10 ? 'round-active' : ''}"></div></a>
+						<a href="${el.id === 11 ? '#' : './engeneer.html'}"><div class="round ${el.id === 11 ? 'round-active' : ''}"></div></a>
+					</nav>
+				</div>
+				<img class="crew__wrapper__img" src="${el.imgSrc}" alt="${el.rank}" />
 			`
 			);
 		}
